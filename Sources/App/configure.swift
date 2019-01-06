0e163eb130ca908e25b35cd1346f43caa87e0370
Janoz94 <Janoz94@users.noreply.github.com>
2019-01-06 16:42:32 +0100

    Project modifications to use Vapor Cloud 2

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,28 +19,44 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     /// Register the configured mySQL database to the database config.
     var databases = DatabasesConfig()
-    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName: String
-    let databasePort: Int
-    // 1
-    if (env == .testing) {
-        databaseName = "vapor-test"
-        if let testPort = Environment.get("DATABASE_PORT") {
-            databasePort = Int(testPort) ?? 5433
-        } else {
-            databasePort = 5433
-        }   
+    let databaseConfig: PostgreSQLDatabaseConfig
+    
+    if let url = Environment.get("DATABASE_URL") {
+        guard let urlConfig = PostgreSQLDatabaseConfig(url: url) else {
+            fatalError("Failed to create PostgresConfig")
+        }
+        databaseConfig = urlConfig
+    } else if let cloud2Url = Environment.get("POSTGRESQL_URL") {
+        guard let urlConfig = PostgreSQLDatabaseConfig(url: cloud2Url) else {
+            fatalError("Failed to create PostgresConfig")
+        }
+        databaseConfig = urlConfig
     } else {
-        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-        databasePort = 5432
+        let databaseName: String
+        let databasePort: Int
+        if (env == .testing) {
+            databaseName = "vapor-test"
+            if let testPort = Environment.get("DATABASE_PORT") {
+                databasePort = Int(testPort) ?? 5433
+            } else {
+                databasePort = 5433
+            }
+        }
+        else {
+            databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+            databasePort = 5432
+        }
+        
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let username = Environment.get("DATABASE_USER") ?? "vapor"
+        let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+        databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password)
     }
-    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
-    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password)
+    
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
-    
+
     /// Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: User.self, database: .psql)
