c37d580293493508e849038370487015531e201e
Napoleonis <janek.rut94@gmail.com>
2019-01-22 13:40:34 +0100

    Started working on OAuth2 implementation

---
+++ Sources/App/Controllers/ImperialController.swift
@@ -0,0 +1,7 @@
+import Vapor
+import Imperial
+import Authentication
+struct ImperialController: RouteCollection {
+    func boot(router: Router) throws {
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -18,9 +18,11 @@ public func routes(_ router: Router) throws {
     let usersController = UsersController()
     let categoriesController = CategoriesController()
     let websiteController = WebsiteController()
+    let imperialController = ImperialController()
     
     try router.register(collection: acronymsController)
     try router.register(collection: usersController)
     try router.register(collection: categoriesController)
     try router.register(collection: websiteController)
+    try router.register(collection: imperialController)    
 }
