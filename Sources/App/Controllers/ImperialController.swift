0b05180cd6647f39341eb7c66c9e47ef503e69ce
Napoleonis <janek.rut94@gmail.com>
2019-01-22 14:55:23 +0100

    Working on OAuth2 implementation

--- Sources/App/Controllers/ImperialController.swift
+++ Sources/App/Controllers/ImperialController.swift
@@ -1,7 +1,13 @@
 import Vapor
 import Imperial
 import Authentication
+import Foundation
+
 struct ImperialController: RouteCollection {
     func boot(router: Router) throws {
+    }
+    
+    func processGoogleLogin(request: Request, token: String) throws -> Future<ResponseEncodable> {
+            return request.future(request.redirect(to: "/"))
     }
 }
