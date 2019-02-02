16674b1ace143caec8f255e8425f513dbb5d491e
Janoz94 <Janoz94@users.noreply.github.com>
2019-02-02 15:56:31 +0100

    Add twitterURL to User and make Categories unique

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -216,7 +216,11 @@ struct WebsiteController: RouteCollection {
         }
         
         let password = try BCrypt.hash(data.password)
-        let user = User(name: data.name, username: data.username, password: data.password)
+        var twitterURL: String?
+        if let twitter = data.twitterURL, !twitter.isEmpty {
+            twitterURL = twitter
+        }
+        let user = User(name: data.name, username: data.username, password: password, twitterURL: twitterURL)
         
         return user.save(on: req).map(to: Response.self) { user in
             try req.authenticateSession(user)
@@ -307,6 +311,7 @@ struct RegisterData: Content {
     let username: String
     let password: String
     let confirmPassword: String
+    let twitterURL: String?
 }
 
 extension RegisterData: Validatable, Reflectable {
